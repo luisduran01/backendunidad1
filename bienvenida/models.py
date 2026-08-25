9a691cdee0ef6bc525153a04a20da8e0fc35a1a3
Duran <luisduran20061@gmail.com>
2026-08-25 13:26:51 -0400

    cambio

--- bienvenida/models.py
+++ bienvenida/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
 class Producto(models.Model):
- nombre = models.CharField(max_length=64)
- precio = models.DecimalField(default=0)
- stock = models.IntegerField(default=0)
- descripcion = models.CharField(max_length=128, null=True, blank=True)
- def __str__(self): 
-  return self.nombre 
+    nombre = models.CharField(max_length=64)
+    precio = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0
+    )
+    stock = models.IntegerField(default=0)
+    descripcion = models.CharField(
+        max_length=128,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.nombre
