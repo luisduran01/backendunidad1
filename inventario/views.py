aeb92745242bfd7de52ed6b5125017f64b1c19d6
Duran <luisduran20061@gmail.com>
2026-09-01 13:29:48 -0400

    s

--- inventario/views.py
+++ inventario/views.py
@@ -1,47 +1,51 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Producto
+from .forms import ProductoForm
 
-# inventario/views.py 
-from django.shortcuts import render, get_object_or_404, redirect 
-from .models import Producto 
-from .forms import ProductoForm 
+from django.views.decorators.csrf import csrf_protect
 
+# Create your views here.
+def producto_list(request):
+    productos = Producto.objects.all()
+    return render(request, 'inventario/producto_list.html', {'object_list': productos})
 
-def producto_list(request): 
-    productos = Producto.objects.all() 
-    return render(request, 'inventario/producto_list.html', 
-{'object_list': productos}) 
+def producto_detail(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'inventario/producto_detail.html', {'object': producto})
 
-def producto_detail(request, pk): 
-    producto = get_object_or_404(Producto, pk=pk) 
-    return render(request, 'inventario/producto_detail.html', 
-{'object': producto})
+@csrf_protect
+def producto_create(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('producto_list')
 
-def producto_create(request): 
-    if request.method == 'POST': 
-        form = ProductoForm(request.POST) 
-        if form.is_valid(): 
-            form.save() 
-            return redirect('producto_list') 
-    else: 
-        form = ProductoForm() 
-    return render(request, 'inventario/producto_form.html', {'form': 
-form})
+    else:
+        form = ProductoForm()
+        return render(request, 'inventario/producto_form.html', {'form': form})
 
-def producto_update(request, pk): 
-    producto = get_object_or_404(Producto, pk=pk) 
-    if request.method == 'POST': 
+@csrf_protect
+def producto_update(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+
+    if request.method == 'POST':
         form = ProductoForm(request.POST, instance=producto)
-        if form.is_valid(): 
-          form.save() 
-        return redirect('producto_list') 
-    else: 
-        form = ProductoForm(instance=producto) 
-    return render(request, 'inventario/producto_form.html', {'form': 
-form})
-def producto_delete(request, pk): 
-    producto = get_object_or_404(Producto, pk=pk) 
-    if request.method == 'POST': 
-        producto.delete() 
-        return redirect('producto_list') 
-    return render(request, 
-'inventario/producto_confirm_delete.html', {'object': producto}) 
- 
+
+        if form.is_valid():
+            form.save()
+            return redirect('producto_list')
+
+    else:
+        form = ProductoForm(instance=producto)
+        return render(request, 'inventario/producto_form.html', {'form': form})
+
+@csrf_protect
+def producto_delete(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('producto_list')
+
+    return render(request, 'inventario/producto_confirm_delete.html', {'object': producto})
