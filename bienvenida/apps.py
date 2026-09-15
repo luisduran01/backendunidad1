fc2615a31caa7a9512675ff78945b27bf1672839
Duran <luisduran20061@gmail.com>
2026-09-15 13:24:56 -0300

    features conect

--- bienvenida/apps.py
+++ bienvenida/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class BienvenidaConfig(AppConfig):
-    name = 'bienvenida'
+class InventarioConfig(AppConfig):
+    name = 'inventario'
--- bienvenida/models.py
+++ bienvenida/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
-    nombre= models.CharField(max_length=64)
-    precio= models.IntegerField(default=0)
-    descripcion= models.CharField(max_length=128, blank=True, null=True)
-    stock= models.IntegerField(default=0)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    precio = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
